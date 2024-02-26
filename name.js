39386ae712b2b85f534c423a4bab366861f15e22
imanwaheed317 <160356928+imanwaheed317@users.noreply.github.com>
2024-02-26 20:46:38 +0500

    Add files via upload

---
+++ name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['imaan', 'sonaina', 'maheen', 'ayesha'];
+const message = 'Welcome to typescript world';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ name.ts
@@ -0,0 +1,10 @@
+let names :string[] = ['imaan','sonaina','maheen','ayesha']
+
+const message :string = 'Welcome to typescript world '
+
+
+for( let i=0; i<names.length; i++){
+
+    console.log(message + names [i]);
+    
+}
